209a8d4749ebef8a63ecc97b5a68a9214ee495d6
Jay <jaymalinchak319@gmail.com>
2018-08-26 05:10:29 -0400

    test push
    test push

--- assets/__resource.lua
+++ assets/__resource.lua
@@ -8,14 +8,13 @@ files {
 	"peds.meta",
 	"carcols.meta",
 	"vehiclelayouts.meta",
-	"handling.meta",
-	"weapons.meta",
-	"weaponanimations.meta"
+	"handling.meta"
 	
 
 	
 
 }
+
 data_file "VEHICLEAIHANDLINGINFO_FILE" "vehicleaihandlinginfo.meta"
 data_file "HANDLING_FILE" "handling.meta"
 data_file "PED_METADATA_FILE" "peds.meta"
@@ -23,7 +22,6 @@ data_file "VEHICLE_METADATA_FILE" "vehicles.meta"
 data_file "CARCOLS_FILE" "carcols.meta"
 data_file "VEHICLE_VARIATION_FILE" "carvariations.meta"
 data_file "VEHICLE_LAYOUTS_FILE" "vehiclelayouts.meta"
-data_file "WEAPONINFO_FILE" "weapons.meta"
-data_file "WEAPON_ANIMATIONS_FILE" "weaponanimations.meta"
+
 
 client_script "vehiclenames.lua"
---
+++ assets/vehiclenames.lua
@@ -0,0 +1,158 @@
+function AddTextEntry( key, value )
+	Citizen.InvokeNative( GetHashKey( "ADD_TEXT_ENTRY" ), key, value)
+end
+
+Citizen.CreateThread( function()
+	--this file defines vehicles edited name for police radar/mdt
+
+	AddTextEntry( "police", "LEO Impala" )
+
+	AddTextEntry( "police2", "LEO 14 Charger" )
+
+	AddTextEntry( "police3", "LEO Taurus" )
+
+	AddTextEntry( "police5", "LEO Taurus" )
+
+	AddTextEntry( "police6", "LEO 2013 Chevy Tahoe K9" )
+
+	AddTextEntry( "police7", "LEO 2011 Crown Victoria" )
+
+	AddTextEntry( "police11", "LEO 2015 Chevy Tahoe" )
+
+	AddTextEntry( "police12", "LEO 2013 Chevy Tahoe" )
+
+	AddTextEntry( "police14", "LEO 2014 Explorer" )
+
+	AddTextEntry( "police15", "LEO 2014 Charger" )
+
+	AddTextEntry( "police16", "LEO 2016 Charger" )
+
+	AddTextEntry( "policeb2", "LEO Harley" )
+
+	AddTextEntry( "polmav", "LEO Helicopter" )
+
+	AddTextEntry( "pranger2", "LEO F350" )
+
+	AddTextEntry( "sheriff", "LEO 2008 Crown Victoria" )
+
+	AddTextEntry( "slickcvpi", "LEO 2011 Crown Victoria UM" )
+
+	AddTextEntry( "lssherifft", "LEO Prison Van" )
+
+	AddTextEntry( "prangeratv", "LEO Police ATV" )
+
+	AddTextEntry( "riot2", "LEO Bearcat" )
+
+	AddTextEntry( "scorcherp", "LEO Police Bicycle" )
+
+	AddTextEntry( "predator", "LEO Police Boat" )
+
+	AddTextEntry( "flatbed2", "Flatbed 2" )
+
+	AddTextEntry( "futo2", "Futo Pizza This" )
+
+	AddTextEntry( "ambulance", "EMS F450 Ambulance" )
+
+	AddTextEntry( "ambulance2", "EMS F250 Ambulance" )
+	
+	AddTextEntry( "policetahoe", "EMS 2015 Chevy Tahoe" )
+
+	
+	AddTextEntry( "buffalo", "Dodge Charger" )
+
+	AddTextEntry( "0xC1AE4D16", "Porsche 911" )
+
+	AddTextEntry( "0x04CE68AC", "Ford Mustang" )
+
+	AddTextEntry( "0xDE3D9D22", "Nissan GTR" )
+
+	AddTextEntry( "0xBF1691E0", "BMW M4" )
+
+	AddTextEntry( "0x9628879C", "Chevy Tahoe" )
+
+	AddTextEntry( "0xA7EDE74D", "Ford Crown Vic" )
+
+	AddTextEntry( "0x779F23AA", "Cadillac Escalade" )
+
+	AddTextEntry( "rancherxl", "1989 GMC Suburban" )
+
+	AddTextEntry( "0xB52B5113", "Mercedes S Class" )
+
+	AddTextEntry( "huntley", "2017 Bentley Bentayga" )
+
+	AddTextEntry( "iroc", "1990 Chevrolet Camaro IROC-Z" )
+
+	AddTextEntry( "f150", "2015 Ford F-150" )
+
+	AddTextEntry( "impalanew", "2006 Chevy Impala" )
+
+	AddTextEntry( "impalaold", "1985 Chevy Impala" )
+
+	AddTextEntry( "bmw760", "BMW 760i" )
+
+	AddTextEntry( "gwagon", "Mercedes-Benz G65 AMG" )
+
+	AddTextEntry( "srt8", "Jeep SRT-8 2015" )
+
+	AddTextEntry( "delta88b", "1973 Oldsmobile Delta 88" )
+
+	AddTextEntry( "suburban", "2016 Chevrolet Suburban" )
+
+	AddTextEntry( "prius", "Toyota Prius" )
+
+	AddTextEntry( "oldram", "1999 Dodge Ram 1500" )
+
+	AddTextEntry( "focus", "2017 Ford Focus RS" )
+
+	AddTextEntry( "fusion", "2017 Ford Fusion" )
+
+	AddTextEntry( "rav4", "Toyota RAV4" )
+
+	AddTextEntry( "camry", "2009 Toyota Camry" )
+
+	AddTextEntry( "executioner", "Vapid Executioner SUV" )
+
+	AddTextEntry( "greenwood", "Schyster Greenwood" )
+
+	AddTextEntry( "sadler4", "Sadler F-150 Single Cab Truck" )
+
+	AddTextEntry( "sandking3", "Sandking F250 Crew Cab Truck" )
+
+	AddTextEntry( "stratum3", "Stratum Coupe" )
+
+	AddTextEntry( "bsfugitive", "Security Fugitive" )
+
+	AddTextEntry( "bsgranger", "Security Granger" )
+
+	AddTextEntry( "primo", "Buick Centry" )
+
+	AddTextEntry( "COQUETTE2", "Corvette" )
+
+    AddTextEntry( "180sx", "Nissan 180sx" )
+
+    AddTextEntry( "350z", "Nissan 370z" )
+
+    AddTextEntry( "zultan", "Mitstubshi Lancer EVO" )
+
+    AddTextEntry( "COQUETTE2", "Corvette" )
+ 
+    AddTextEntry( "mfc", "Mazda RX-7" )
+
+    AddTextEntry( "integra", "Honda Integra" )
+
+    AddTextEntry( "jzx", "Toyota Chaser JZX-100" )
+
+    AddTextEntry( "na6", "Mazda Miata" )
+
+    AddTextEntry( "fd22", "Mazda RX-7 FD" )
+
+    AddTextEntry( "Skyline", "Nissan Skyline" )
+
+    AddTextEntry( "s15", "Nissan S15" )
+
+    AddTextEntry( "supra2", "Toyota Supra" )
+
+    AddTextEntry( "Neon", "Tesla Model S" )
+	
+	
+end)
